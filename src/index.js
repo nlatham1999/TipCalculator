bd243bc23cefad853de78ee075f4b2591f0d81e6
nick Latham <nlatham@zagmail.gonzaga.edu>
2020-10-09 11:45:02 -0700

    uploading the project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './App.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class TipCalculator extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    constructor(props) {
+      super(props);
+      this.state = {
+          bill: 0,
+          tip: 0,
+      }
+    }
+
+    myChangeHandler = (event) => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
+      }
+
+    render() {
+        let tipAmount = this.state.bill * (this.state.tip / 100);
+        const total = tipAmount + Number(this.state.bill);
+
+      return (
+        <form>
+            <h1>Tip Calculator</h1>
+        <label for='bill'>Enter your bill amount:&emsp;&emsp;$</label>
+        <input
+            class='input'
+            type='number'
+            name='bill'
+            onChange={this.myChangeHandler}
+        />
+        <br></br>
+        <label for='tip'>Enter your tip percentage: %</label>
+        <input 
+            class='input'
+            type='number'
+            name='tip'
+            onChange={this.myChangeHandler}
+        />
+        <p>Your tip amount is ${tipAmount.toFixed(2)}</p>
+        <p>Your total bill is ${total.toFixed(2)}</p>
+        </form>
+      );
+    }
+  }
+  
+  ReactDOM.render(<TipCalculator />, document.getElementById('root'));
